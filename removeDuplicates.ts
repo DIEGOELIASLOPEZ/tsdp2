92204cfd18c501cdebeb5bea2c60fb53f3273e39
Elias López Martínez <91902204+DIEGOELIASLOPEZ@users.noreply.github.com>
2025-04-10 23:23:39 -0600

    Update removeDuplicates.ts

--- removeDuplicates.ts
+++ removeDuplicates.ts
@@ -2,7 +2,7 @@ function removeDuplicates(nums: number[]): number {
     let k = 0;
 
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[k]) {
+        if (nums[i] > nums[k]) {
             k++;  
             nums[k] = nums[i];  
         }
